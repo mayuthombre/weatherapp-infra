fb7de71ab15e18a87b856976a3d8ae22c2381347
mayuthombre <mayu.thombre@contino.io>
2022-09-09 17:41:57 +1000

    changed tfstate file name

--- infrastructure/meta.tf
+++ infrastructure/meta.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.2.0"
   backend "s3" {
     bucket = "pathways-dojo"
-    key    = "mayu-tfstate-infra"
+    key    = "mayu-tfstate-main"
     region = "us-east-1"
   }
 }
