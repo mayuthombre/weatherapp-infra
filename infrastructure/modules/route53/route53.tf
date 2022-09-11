6b7ba6c50fb3cf538067c56179d5269db6137578
mayuthombre <mayu.thombre@contino.io>
2022-09-12 09:28:24 +1000

    create route53 record

--- infrastructure/modules/route53/route53.tf
+++ infrastructure/modules/route53/route53.tf
@@ -16,45 +16,45 @@ resource "aws_route53_record" "domain" {
   }
 }
 
-# Generage certificate for domain name using ACM
-resource "aws_acm_certificate" "certificate" {
-  domain_name       = aws_route53_record.domain.name
-  validation_method = "DNS"
+# # Generage certificate for domain name using ACM
+# resource "aws_acm_certificate" "certificate" {
+#   domain_name       = aws_route53_record.domain.name
+#   validation_method = "DNS"
 
-  depends_on = [
-    aws_route53_record.domain
-  ]
-}
+#   depends_on = [
+#     aws_route53_record.domain
+#   ]
+# }
 
 
-# Create record entry for CNAME in the hosted zone weatherapp.click
-resource "aws_route53_record" "cert" {
-  for_each = {
-    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
-      name   = "${dvo.resource_record_name}"
-      record = "${dvo.resource_record_value}"
-      type   = "${dvo.resource_record_type}"
-    }
-  }
+# # Create record entry for CNAME in the hosted zone weatherapp.click
+# resource "aws_route53_record" "cert" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+#       name   = "${dvo.resource_record_name}"
+#       record = "${dvo.resource_record_value}"
+#       type   = "${dvo.resource_record_type}"
+#     }
+#   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = each.value.record
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.primary.zone_id
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = each.value.record
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.primary.zone_id
 
-  depends_on = [
-    aws_route53_record.domain
-  ]
-}
+#   depends_on = [
+#     aws_route53_record.domain
+#   ]
+# }
 
-# Certificate validation
-resource "aws_acm_certificate_validation" "validation" {
-  certificate_arn         = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
+# # Certificate validation
+# resource "aws_acm_certificate_validation" "validation" {
+#   certificate_arn         = aws_acm_certificate.certificate.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
 
-  depends_on = [
-    aws_route53_record.cert
-  ]
-}
+#   depends_on = [
+#     aws_route53_record.cert
+#   ]
+# }
