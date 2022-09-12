617d806240addfd399ebe32a217c545e37550e78
mayuthombre <mayu.thombre@contino.io>
2022-09-12 17:20:05 +1000

    region change from us-east-1 to ap-southeast-2

--- infrastructure/modules/ecr/ecr.tf
+++ infrastructure/modules/ecr/ecr.tf
@@ -1,10 +1,6 @@
-# module "locals" {
-#   source = "../locals"
-# }
-
 # Create ECR repository
 resource "aws_ecr_repository" "weatherapp_ecrrepo" {
-  name         = "${var.name}-repo" # Naming my repository
+  name         = "mayu-weatherapp-repo" # Naming my repository
   force_delete = true               # enabling this feature so that github actions can delete repo with images.           
 
   image_scanning_configuration {
