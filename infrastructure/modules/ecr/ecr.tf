6da16337b6355231e017dbd04e2ec9b7f8a2c21f
mayuthombre <111035380+mayuthombre@users.noreply.github.com>
2022-09-12 17:32:06 +1000

    Update ecr.tf

--- infrastructure/modules/ecr/ecr.tf
+++ infrastructure/modules/ecr/ecr.tf
@@ -4,7 +4,7 @@
 
 # Create ECR repository
 resource "aws_ecr_repository" "weatherapp_ecrRepo" {
-  name         = "${var.name}-repo" # Naming my repository
+  name         = "mayu-weatherapp-repo" # Naming my repository
   force_delete = true               # enabling this feature so that github actions can delete repo with images.           
 
   image_scanning_configuration {
