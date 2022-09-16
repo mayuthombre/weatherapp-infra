3e64681d2e41c55019d4650ebe6b9efbd4baeb0d
mayuthombre <mayu.thombre@contino.io>
2022-09-16 18:35:44 +1000

    removed subnet IDs from s3 endpoint & added ecs subnets

--- infrastructure/modules/vpc/endpoint.tf
+++ infrastructure/modules/vpc/endpoint.tf
@@ -3,7 +3,6 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.vpc.id
   service_name = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
-  subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id, aws_subnet.private_subnet_c.id]
   
   tags = merge(
     var.tags,
@@ -17,6 +16,7 @@ resource "aws_vpc_endpoint" "ecs" {
   vpc_id       = aws_vpc.vpc.id
   service_name = "com.amazonaws.${var.region}.ecs"
   vpc_endpoint_type = "Interface"
+  subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id, aws_subnet.private_subnet_c.id]
 
   tags = merge(
     var.tags,
