3850bb2a4bcb3208b6cf1ac0aa724b4cea0b2f9a
mayuthombre <mayu.thombre@contino.io>
2022-09-16 18:47:07 +1000

    added route tables

--- infrastructure/modules/vpc/endpoint.tf
+++ infrastructure/modules/vpc/endpoint.tf
@@ -3,16 +3,29 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.vpc.id
   service_name = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
+  route_table_ids = [
+    aws_route_table.private_rt_a.id, 
+    aws_route_table.private_rt_b.id, 
+    aws_route_table.private_rt_c.id
+    ]
   
-  tags = var.tags
+  tags = merge(
+    var.tags,
+    {
+        Name = "${var.name}-s3"
+    }
+  )
 }
 
-# Create ECS endpoint
 resource "aws_vpc_endpoint" "ecs" {
   vpc_id       = aws_vpc.vpc.id
   service_name = "com.amazonaws.${var.region}.ecs"
   vpc_endpoint_type = "Interface"
-  subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id, aws_subnet.private_subnet_c.id]
+  subnet_ids = [
+    aws_subnet.private_subnet_a.id, 
+    aws_subnet.private_subnet_b.id, 
+    aws_subnet.private_subnet_c.id
+    ]
 
   tags = merge(
     var.tags,
