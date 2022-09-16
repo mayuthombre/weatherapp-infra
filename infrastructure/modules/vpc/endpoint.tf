02570e8469d1568085d65556e2ceec0387ae2f41
mayuthombre <mayu.thombre@contino.io>
2022-09-16 17:27:51 +1000

    created endpoint.tf

--- infrastructure/modules/vpc/endpoint.tf
+++ infrastructure/modules/vpc/endpoint.tf
@@ -1,7 +1,7 @@
 # Create endpoint for ECS to pull image directly from the ECR without transversing internet to reduce cost
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.vpc.id
-  service_name = "com.amazonaws.[${var.region}].s3"
+  service_name = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Interface"
 
   tags = var.tags
