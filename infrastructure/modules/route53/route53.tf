10ec132d735561b2eb8db2543332590542b3f7bf
mayuthombre <mayu.thombre@contino.io>
2022-09-19 09:30:55 +1000

    ACM certificate creation test2

--- infrastructure/modules/route53/route53.tf
+++ infrastructure/modules/route53/route53.tf
@@ -26,36 +26,36 @@ resource "aws_acm_certificate" "certificate" {
   ]
 }
 
-# # Certificate validation
-# resource "aws_acm_certificate_validation" "validation" {
-#   certificate_arn         = aws_acm_certificate.certificate.arn
-#   validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
 
-#   depends_on = [
-#     aws_route53_record.cert
-#   ]
-# }
+# Create record entry for CNAME in the hosted zone weatherapp.click
+resource "aws_route53_record" "cert" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = each.value.record
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.primary.zone_id
+
+  depends_on = [
+    aws_route53_record.domain
+  ]
+}
 
 
-# # Create record entry for CNAME in the hosted zone weatherapp.click
-# resource "aws_route53_record" "cert" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
-#       name   = "${dvo.resource_record_name}"
-#       record = "${dvo.resource_record_value}"
-#       type   = "${dvo.resource_record_type}"
-#     }
-#   }
+# Certificate validation
+resource "aws_acm_certificate_validation" "validation" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = each.value.record
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = data.aws_route53_zone.primary.zone_id
-
-#   depends_on = [
-#     aws_route53_record.domain
-#   ]
-# }
-
+  depends_on = [
+    aws_route53_record.cert
+  ]
+}
