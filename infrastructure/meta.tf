2cbf24060342fe7f5d867fb6bb2b0e2cd5def7a7
mayuthombre <mayu.thombre@contino.io>
2022-11-07 14:54:11 +1100

    changed lock state file name in s3

--- infrastructure/meta.tf
+++ infrastructure/meta.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13.0"
   backend "s3" {
     bucket = "pathways-dojo"
-    key    = "mayu-tfstate-blue"
+    key    = "mayu-tfstate-main"
     region = "us-east-1"
   }
 }
